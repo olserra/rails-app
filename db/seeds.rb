9f5a8af444d99f6b589f63ead8755dc7ab3185ca
Otavio Serra <otavio.serra@gaspardbruno.com>
2022-03-04 14:35:47 +0000

    1st API added for products, and seeds as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+    {
+        name: 'iPhone X',
+        brand: 'Apple',
+        price: '$999.99',
+        description: 'iPhone X is the latest addition to the iPhone line-up. It features a 5.8-inch Super Retina display, a new A12 Bionic chip, and a new 12MP camera. It is powered by a 2.84GHz dual-core Apple A12X chip with M12 motion coprocessor.'
+    },
+    {
+        name: 'iPhone XS',
+        brand: 'Apple',
+        price: '$999.99',
+        description: 'iPhone XS is the latest addition to the iPhone line-up. It features a 5.8-inch Super Retina display, a new A12 Bionic chip, and a new 12MP camera. It is powered by a 2.84GHz dual-core Apple A12X chip with M12 motion coprocessor.'
+    },
+    {
+        name: 'iPhone XR',
+        brand: 'Apple',
+        price: '$999.99',
+        description: 'iPhone XR is the latest addition to the iPhone line-up. It features a 5.8-inch Super Retina display, a new A12 Bionic chip, and a new 12MP camera. It is powered by a 2.84GHz dual-core Apple A12X chip with M12 motion coprocessor.'
+    },
+    {
+        name: 'iPhone XS Max',
+        brand: 'Apple',
+        price: '$999.99',
+        description: 'iPhone XS Max is the latest addition to the iPhone line-up. It features a 5.8-inch Super Retina display, a new A12 Bionic chip, and a new 12MP camera. It is powered by a 2.84GHz dual-core Apple A12X chip with M12 motion coprocessor.'
+    },
+])
